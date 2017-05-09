75a73ff3fe919c0977fd9576125a3da54a4cace3
amiserda <aronmiserda@gmail.com>
2017-05-09 18:44:29 +0200

    add dll and test web project

---
+++ WebApiToTypeScript/WebApiToTypeScript.TestWebApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebApiToTypeScript.TestWebApp.WebApiApplication" Language="C#" %>
---
+++ WebApiToTypeScript/WebApiToTypeScript/TypeScriptGenerationTask.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Build.Utilities;
+
+namespace WebApiToTypeScript
+{
+    public class TypeScriptGenerationTask : AppDomainIsolatedTask
+    {
+        public override bool Execute()
+        {
+            return true;
+        }
+    }
+}
